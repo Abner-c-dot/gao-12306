30b422ea9bbc0a05d001345889b5240cd058dd7d
王霄霄 <wangxiaoxiaorz@howso.cn>
2023-11-08 10:14:39 +0800

    feat：framework重构结构创建

---
+++ frameworks/log/src/test/java/org/opengoofy/index12306/framework/starter/log/LogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.opengoofy.index12306.framework.starter.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
